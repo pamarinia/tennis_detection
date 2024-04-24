50ed43a9441b1eae013f47609f185829ea2f517a
pamarinia <paul.marin@etu.ec-lyon.fr>
2024-04-24 19:32:58 +0200

    draw stats

--- utils/__init__.py
+++ utils/__init__.py
@@ -7,3 +7,4 @@ from .bbox_utils import (get_center_of_bbox,
                          measure_xy_distance,
                          get_center_of_bbox)
 from.conversions import convert_pixel_distance_to_meters, convert_meters_to_pixel_distance
+from .player_stats_drawer_utils import draw_player_stats
